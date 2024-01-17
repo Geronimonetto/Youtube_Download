492d8a4efc31fa050ae07a7ab0ab35c1a5bed72c
geronimo morais <geronimomorais1617@gmail.com>
2024-01-17 20:15:35 -0300

    Youtube_Download - Ajustando arquivos para executável, inserindo funções de download para formato mp4 e ajustando para download em formato mp3

--- main/app.py
+++ main/app.py
@@ -1,8 +1,20 @@
-from mhyt import yt_download
+from pytube import YouTube
+import os 
 
-url ="https://youtu.be/T2BQG_8CFHI?si=a9o-CLhbg0Uso01F"
-file = "Musica.mp4"
-yt_download(url, file)
 
-#file = "Musica.mp3"
-#yt_download(url, file, ismusic=True)
+youtube_downloader = YouTube('https://youtu.be/T2BQG_8CFHI?si=a9o-CLhbg0Uso01F')
+movie = youtube_downloader.streams.get_highest_resolution()
+movie.download()
+
+#Download to format mp3
+song = youtube_downloader.streams.filter(only_audio=True).first()
+out_file = song.download()
+
+#Save file format mp3
+base, ext = os.path.splitext(out_file)
+new_file = base + ".mp3"
+os.rename(out_file, new_file)
+
+#Return Sucess
+print(youtube_downloader.title + " Sucess Download")
+
